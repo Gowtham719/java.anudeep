d6571ed861e6c6f8173ee76591d5ae6be37df0fb
Gowtham719 <gowthamgowdagowtham719@gmail.com>
2025-12-05 20:44:08 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float r=10.2f;
+		double area=3.14*r*r;
+		System.out.println("area="+area);
+	}
+
+}
