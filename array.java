551c0182791a717b0d05b400d5a1e035e392f10b
Gowtham719 <gowthamgowdagowtham719@gmail.com>
2025-12-05 20:36:34 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,19 @@
+package arraydemo;
+
+import java.util.Iterator;
+
+public class array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[];
+		arr = new int[6];
+		for(int i=0; i< arr.length; i++) {
+			arr[i] = i*3;
+		}
+		for(int i=0; i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
