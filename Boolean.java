3452ea57a31a884f264f7b0585865b7213c1f8f9
Gowtham719 <gowthamgowdagowtham719@gmail.com>
2025-12-05 20:43:11 +0530

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		boolean a=true;
+		if(a==false) {
+			System.out.println(true);
+		}else {
+				System.out.println(false);
+		}
+	}
+
+}
