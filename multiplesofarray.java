2c5bd577f47876c2cbe4cdec6ac66c8a170cc93b
Gowtham719 <gowthamgowdagowtham719@gmail.com>
2025-12-05 20:40:19 +0530

    Add files via upload

---
+++ multiplesofarray.java
@@ -0,0 +1,34 @@
+package arraydemo;
+import java.util.Scanner;
+
+public class multiplesofarray {
+
+    public static void printMultiples(int arr[]) {
+        int count = 0;
+
+        for (int num : arr) {
+            if (num % 3 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        printMultiples(arr);  
+    }
+}
+
